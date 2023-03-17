d67d05489785eca57c48f87bb2f092f0ea708a29
malin <mal3eine@hotmail.com>
2023-03-17 14:50:32 +0100

    stars

--- js/script.js
+++ js/script.js
@@ -6,23 +6,19 @@ arrow.addEventListener('click', function(){
    window.scrollTo({ top: 0, behavior: "smooth"}) 
 })
 
-
-window.addEventListener('scroll', function() { //self-function utan namn
-    //hämta vår position
+//scroll-function//self-function utan namn
+window.addEventListener('scroll', function() { 
+    //get position
     const scrollPosY = window.scrollY
 
     if(scrollPosY >= 100) {
-        arrow.style.display = "block"  //gör till ett blockelement
-        //arrow.classList.remove('hide')
-        //pilen ska ej vara responsiv - behöver inte använda klassen
+        arrow.style.display = "block";
     } else {
-        arrow.style.display = "none"  //dölj 
-        //arrow.classList.add('hide')
+        arrow.style.display = "none";  
     }
-})   //lyssna på ett element. BOM browser object model - webbfönstret
+})   
 
 
-//kan vara en separat fil för de filerna som ska ha trycatch
 try {
     const toggleButton = document.querySelector('[data-option="toggle"]') 
     //vad vill vi hämta:
@@ -42,13 +38,25 @@ try {
 
 } catch {}
 
+
+//star rating*******************************************************************************
+function createStarRating(rating) {
+    //                                      callback function - för varje element i stars
+    const stars = Array.from({ length: 5 }, (_, index) => {
+        const starClass = index < rating ? "fa-solid" : "fa-regular";
+        return `<i class="fa-sharp fa-star ${starClass}"></i>`;
+    });
+  
+    return stars.join("");
+  }
+  
+
 //HÄMTA PRODUKTER
 async function getProducts(target, tag) {
     const element = document.querySelector(target)
 
     const res = await fetch(`https://kyh-net22.azurewebsites.net/api/products/${tag}`)
     const data = await res.json()
-    
 
     for(let item of data) {
         element.innerHTML +=
@@ -73,11 +81,7 @@ async function getProducts(target, tag) {
                 <p class="cathegory">${item.category}</p>
                 <p class="title">${item.name}</p>
                 <div class="rating">
-                    <i class="fa-solid fa-sharp fa-star"></i>
-                    <i class="fa-solid fa-sharp fa-star"></i>
-                    <i class="fa-solid fa-sharp fa-star"></i>
-                    <i class="fa-solid fa-sharp fa-star"></i>
-                    <i class="fa-regular fa-sharp fa-star"></i>
+                    ${createStarRating(item.starRating)}
                 </div>
                 <p class="price">${item.originalPrice} ${item.currency}</p>
             </div> 
@@ -120,11 +124,7 @@ async function getAllProducts(target) {
                 <p class="cathegory">${item.category}</p>
                 <p class="title">${item.name}</p>
                 <div class="rating">
-                    <i class="fa-solid fa-sharp fa-star"></i>
-                    <i class="fa-solid fa-sharp fa-star"></i>
-                    <i class="fa-solid fa-sharp fa-star"></i>
-                    <i class="fa-solid fa-sharp fa-star"></i>
-                    <i class="fa-regular fa-sharp fa-star"></i>
+                    ${createStarRating(item.starRating)}
                 </div>
                 <p class="price">${item.originalPrice} ${item.currency}</p>
             </div> 
@@ -138,25 +138,25 @@ async function getAllProducts(target) {
 
 //SKICKA TILL API
 
-async function handleContactForm(e) {
-    e.preventDefault()
+// async function handleContactForm(e) {
+//     e.preventDefault()
 
-    const form = {
-        name: "Malin",
-        email: "mal3eine@hotmail.com",
-        comments: "Jag vill bli kontaktad."
-    }
+//     const form = {
+//         name: "Malin",
+//         email: "mal3eine@hotmail.com",
+//         comments: "Jag vill bli kontaktad."
+//     }
     
-    // skicka formuläret:
-    const res = await fetch('https://kyh-net22.azurewebsites.net/api/contacts', {
-        method: 'post',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify(form)
-    })
+//     // skicka formuläret:
+//     const res = await fetch('https://kyh-net22.azurewebsites.net/api/contacts', {
+//         method: 'post',
+//         headers: {
+//             'Content-Type' : 'application/json'
+//         },
+//         body: JSON.stringify(form)
+//     })
     
-    if(res.status === 200)
-        console.log('tack för din förfrågan!')
+//     if(res.status === 200)
+//         console.log('tack för din förfrågan!')
 
-}
+// }
--- js/validate.js
+++ js/validate.js
@@ -2,52 +2,7 @@
 const nameError = document.getElementById('name-error')
 const emailError = document.getElementById('email-error')
 const commentsError = document.getElementById('comments-error')
-const sumbitError = document.getElementById('submit-error')
-
-// //submit form
-// const handleSubmit = (e) => {
-//     e.preventDefault()
-//     const errors = []
-
-//     for(let element of e.target) {
-//         if (element.required) {
-//             switch(element.type) {
-//                 case 'email':
-//                     if (!validateEmail(element.value))
-//                         errors.push(element)
-//                     break;
-//                 case 'text':
-//                     if (!validateName(element.value))
-//                         errors.push(element)
-//                     break;
-//                 case 'password':
-//                     if (!validatePassword(element.value))
-//                         errors.push(element)
-//                     break; 
-//             }
-//         }
-//     }
-
-//     if(errors.length == 0)
-//         window.location.reload('success.html')
-// }
-
-//funktion som validerar det som skickas in i funktionen
-// function validate(event){
-//     switch(event.target.type) {
-//         case 'name':
-//             validateName(event.target.value)
-//             break;
-//         case 'email':
-//             validateEmail(event.target.value)
-//             break;
-//         case 'password':
-//             validatePassword(event.target.value)
-//             break;
-//     }
-// }
-
-//validera namn
+const submitError = document.getElementById('submit-error')
 
 function validateName(){
     const name = document.getElementById('name').value;
@@ -63,22 +18,20 @@ function validateName(){
     return true;
 }
 
-
 function validateEmail(){
     const email = document.getElementById('email').value;
 
     if(email.length == 0){
         emailError.innerHTML ='Email is required';
-        return false
+        return false;
     }    
-    if(!email.match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)){
-        emailError.innerHTML = 'Email invalid';
+    if(!/^[\w.%+-]+@[\w.-]+\.[A-Za-z]{2,}$/i.test(email)){
+        emailError.innerHTML = 'Invalid email';
         return false;
     }
     emailError.innerHTML = '<i class="fas fa-check"></i>';
     return true;
 }
-
 
 function validateComments(){
     const comments = document.getElementById('comments').value;
@@ -88,77 +41,81 @@ function validateComments(){
         commentsError.innerHTML = left + ' more characters required';
         return false;
     }
-    commentsError.innerHTML = '<i class="fas fa-check"></i>';
-    return true;
+    else {
+        commentsError.innerHTML = '<i class="fas fa-check"></i>';
+        return true;
+    }
 }
-
 
 function validateForm(){
     if(!validateName() || !validateEmail() || !validateComments()){
-        sumbitError.innerHTML = 'Fill in all columns before submitting the form';
+        submitError.innerHTML = 'Fill in all columns before submitting the form';
 
         return false;
     }
-    sumbitError.innerHTML = 'Comment sent!';
+    submitError.innerHTML = 'Comment sent!';
     return true;
 }
 //_________________________________________________________________________
 
 async function handleContactForm(e) {
-    e.preventDefault()  //förhindra standardbeteende
-    const errors = []   //listar false
-    const errorMessage = document.getElementById('errorMessage')    //hämtar elementet errormessage
+    e.preventDefault()
+    const errors = []   
+    const errorMessage = document.getElementById('submit-error')
     errorMessage.innerHTML = ''
 
     for(let element of e.target) {
-        if(element.required) {  //skapa en errorlist - för varje element som är required läggs det till i listan
+        if(element.required) {  
             const errorElement = document.getElementById(`${element.id}-error`)
-            
-            if (element.value.length === 0) {               //om elementets längd är 0 - skriv ut att elementet är required
+            if (element.value.length === 0) {              
                 errorElement.innerHTML = `${element.id} is required.`
-                errors.push(false)                              //skicka in en false i errors-listan
-            } else {                                           //annars - skriv inte ut något
+                errors.push(false)                              
+            } else {                                           
                 errorElement.innerHTML = ``
-
-                switch(element.type) {
-                    case 'name':                        //lagt till case name
+                switch(element.id) {
+                    case 'name':                       
                         errors.push(validateName())
                         break;
-                    case 'email':                              //beroende på vad det är för typ av element 
-                        errors.push(validateEmail())        //validera elementet - skicka true eller false till errors
+                    case 'email':                              
+                        errors.push(validateEmail())        
                         break
                     case 'comments':
-                        errors.push(validateComments())   //tagit bort comments från mellan parenteserna i funktionen
+                        errors.push(validateComments())   
                         break
                 }
             }
         }
     }
-
-    if (!errors.includes(false)) {              //om errors inte innehåller false- värden
- 
+    if (!errors.includes(false)) {           
         const data = {
             name: e.target['name'].value,
             email: e.target['email'].value,
             comments: e.target['comments'].value
         }
+        //send form
+        try {
+            const res = await fetch('https://kyh-net22.azurewebsites.net/api/contacts', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `bearer ${sessionStorage.getItem("accessToken")}`
+                },
+                body: JSON.stringify(data)
+            });
+            if (res.status === 200) {
+                        
+                const result = await res.text()
+                sessionStorage.setItem('accessToken', result)
+                e.target.reset();
+            } 
+            else {
+                errorMessage.innerHTML = 'Error submitting form.'
+            }
 
-            //skicka formuläret:
-        const res = await fetch('https://kyh-net22.azurewebsites.net/api/contacts', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-                // 'Autorization': `bearer ${sessionStorage.getItem("accessToken")}`
-            },
-            body: JSON.stringify(data)
-        })
-
-        if (res.status === 200) {
-           const result = await res.text()
-           sessionStorage.setItem('accessToken', result)
-
-        } else {
-            errorMessage.innerHTML = 'Incorrect email or password'
+        }
+        catch(error){
+            errorMessage.innerHTML = 'Error submitting form.'
         }
     }
 }
+
